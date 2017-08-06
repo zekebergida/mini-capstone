eb471fd2d5b7ce39629ab2328325523243eac1ca
zekebergida <zekebergida@gmail.com>
2017-08-06 10:55:34 -0400

    create add product feature

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,4 +5,19 @@ class ProductsController < ApplicationController
     render 'full_product_line.html.erb'
   end
 
+  def add_product
+  end
+
+  def save_product
+    new_product = Product.new(
+      name: params[:name],
+      price: params[:price],
+      description: params[:description],
+      image: params[:image]
+      )
+    new_product.save
+    @full_line = Product.all
+    render 'full_product_line.html.erb'
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   get '/all-products' => 'products#all_products'
+  get 'add-product' => 'products#add_product'
+  post 'save-product' => 'products#save_product'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
